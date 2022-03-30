a8290d208557a1034a08c9bec61ae10392f5b70e
AmrutaI <102659919+AmrutaI@users.noreply.github.com>
2022-03-30 15:35:43 +0530

    Add files via upload
    Uploading Activity1

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package Activities;
+
+public class Activity1 {
+
+    public static void main(String []ar) {
+
+        Car nexon = new Car();
+        nexon.make=2014;
+        nexon.color="Black";
+        nexon.transmission="Manual";
+
+        nexon.displayCharacteristics();
+        nexon.accelerate();
+        nexon.brake();
+    }
+}
